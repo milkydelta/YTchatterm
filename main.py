c7059881eaffa2ca05db862f075cdd25d6b2e1f6
milkydelta <88966938+milkydelta@users.noreply.github.com>
2025-10-17 21:57:35 +0100

    Add from py/further/main.py (2025-08-22 to 2025-08-23)

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import time
 import json
 import traceback
 
+
+# This function name is self explanatory, but it only works for regular chat messages.
+# Superchats and those big green messages that members sometimes send will not be printed.
+# If you pass in one of those actions, it will throw an exception.
 def actiontomessage(action):
     message=""
     
@@ -17,7 +21,6 @@ def actiontomessage(action):
             message += run["text"]
         elif "emoji" in run:
             if "isCustomEmoji" in run["emoji"]: #check for channel-specific emoji
-                message += "CustEmoji"
                 message += run["emoji"]["shortcuts"][0]
                 continue
             message += run["emoji"]["emojiId"]
@@ -34,56 +37,65 @@ def actiontomessage(action):
 #videoid = 'GbDP3OGOZQI' #RAT GAMING
 #videoid='cRcbP49Whks' #ROCK GAMING
 #videoid='lopZYsUlvVs' #ROCK&RAT Watch Redline
-videoid='9TWGD7d_lW4' #ROCK&RAT Smash
-ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0'
-#ua= 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+#videoid='9TWGD7d_lW4' #ROCK&RAT Smash
+#videoid='Ih1gKruIga8' #K9 somethingorother i only caught the end
+#videoid='aJ_U6Ne1TG4' # Penrose Picross
+videoid="X_u8nARoQ4g" # Penrose on Saturday (hasn't started yet)
+
 
 url = "https://www.youtube.com/live_chat?v=" + videoid
 
-r = requests.get(url, headers={"user-agent": ua})
+ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0'
 
+
+
+r = requests.get(url, headers={"user-agent": ua})
 initial = r.text
 
 try:
-    continuation = re.findall('continuation":"(.+?)"', initial)[0]
-    #continuation=continuation[16:-1]
-    continuation=continuation.replace("%3D","=")
-    ##print(continuation)
+    continuation_token = re.findall('continuation":"(.+?)"', initial)[0]
+    continuation_token=continuation_token.replace("%3D","=")
 except Exception as e:
     print(e)
     print("BUGGER!")
     print(re.findall('continuation":"(.+?)"', initial))
     with open("bugger.html", "w") as text_file:
         text_file.write(r.text)
-    print("if 'index out of range', i could not find a continuation when I queried the live chat page. Are you sure that the video ID is for a *current* livestream? I dumped the page to 'bugger.html'.")
+    print("I could not find the initial continuation token. Are you sure the video ID is for a current livestream?")
+    print("Page dumped to 'bugger.html'. ")
     exit()
+
 
 #The "key" is the api key that the youtube website uses to talk to the internal YouTube API. It is not an API key for my personal account.
 #If it stops working, the new key would be inside the html page we fetch above. It's called INNERTUBE_API_KEY somewhere in the JS.
-#conturl="https://www.youtube.com/youtubei/v1/live_chat/get_live_chat?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"  #parsing prettyprinted might have a performance impact, but it's easier to debug.
 conturl="https://www.youtube.com/youtubei/v1/live_chat/get_live_chat?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false"
 
 while True:
-    #print("loopBegins")
 
 
-    contpayload={"context":{"client":{"clientName":"WEB","clientVersion":"2.9999099"}},"continuation":continuation}
+    contpayload={"context":{"client":{"clientName":"WEB","clientVersion":"2.9999099"}},"continuation":continuation_token}
 
     time.sleep(0.2)
     r2 = requests.post(conturl, json=contpayload)
 
     contResp = r2.json()
-    ##print(contResp["continuationContents"]["liveChatContinuation"]["continuations"][0]["invalidationContinuationData"]["continuation"])
+
+    # 2025-08-23 00:03 //I analysed a DARN dump from a stream that hadn't started yet. Apparently, those use a different structure for the continuation part of the json response.
+    #                    Running streams use invalidationContinuation and waiting streams use timedContinuation. I should probably rework this to cope with both at the same time.
     try:
-        continuation = contResp["continuationContents"]["liveChatContinuation"]["continuations"][0]["invalidationContinuationData"]["continuation"]
+        continuation_token = contResp["continuationContents"]["liveChatContinuation"]["continuations"][0]["invalidationContinuationData"]["continuation"]
+        #continuation_token = contResp["continuationContents"]["liveChatContinuation"]["continuations"][0]["timedContinuationData"]["continuation"]
     except Exception as e:
         print(e)
         print("DARN!")
+        print("I could not find the next continuation token. The stream probably ended.")
+        print("Response dumped to 'DARN.json'. ")
+        print("Below is what we sent in the request")
         print(contpayload)
         with open("DARN.json", "w") as text_file:
            text_file.write(r2.text)
         exit()
-    continuation=continuation.replace("%3D","=")
+    continuation_token=continuation_token.replace("%3D","=")
 
     try:
         actions=contResp["continuationContents"]["liveChatContinuation"]["actions"]
@@ -95,24 +107,31 @@ while True:
         try:
 
             if "addChatItemAction" in action:
-                if "liveChatPaidMessageRenderer" in action["addChatItemAction"]["item"]: #Paid Messages hava a different JSON message to regular chats. I'll decipher later.
+                if "liveChatPaidMessageRenderer" in action["addChatItemAction"]["item"]: #superchats hava a different JSON message to regular chats. I'll decipher later.
                     cashman_name=action["addChatItemAction"]["item"]["liveChatPaidMessageRenderer"]["authorName"]["simpleText"]
                     print(cashman_name+ " PAID SOME MONEY FOR A MESSAGE.")
-                    continue
                 elif "liveChatMembershipItemRenderer" in action["addChatItemAction"]["item"]: #same goes for the big messages that members sometimes send.
                     member_name=action["addChatItemAction"]["item"]["liveChatMembershipItemRenderer"]["authorName"]["simpleText"]
                     print("MEMBER "+member_name+" HAS SENT A BIG MESSAGE. I'M NOT SAYING WHAT IT IS.")
-                    continue
-                author_name=action["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["authorName"]["simpleText"]
-                message_id= action["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["id"]
-                timestamp= action["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampUsec"]
-                print(timestamp+"  "+message_id[0:15]+"  "+author_name.ljust(15, ' ')+":   "+actiontomessage(action)[0:180])
-            elif "addBannerToLiveChatCommand" in action:
+                elif "liveChatPlaceholderItemRenderer" in action["addChatItemAction"]["item"]:# found this during a test on 2025-08-22. I'm not sure what it is. The exception meant I couldnt crosscheck the id with later msgs
+                    print("PLACEHOLDER!   ID:"+action["addChatItemAction"]["item"]["liveChatPlaceholderItemRenderer"]["id"])
+                else: # regular chat messages
+                    txtmsg = action["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]
+                    author_name = txtmsg["authorName"]["simpleText"]
+                    message_id  = txtmsg["id"]
+                    timestamp   = txtmsg["timestampUsec"]
+                    print(timestamp+"  "+message_id[0:15]+"  "+author_name.ljust(15, ' ')+":   "+actiontomessage(action)[0:180])
+            elif "addBannerToLiveChatCommand" in action: # It's been long enough that I've forgotten what banners are.
                 print("BANNER DETECTED! SKIPPING!")
-            elif "removeChatItemAction" in action:
-                print('{"removeChatItemAction": {"targetItemId": "SomeItemID"}}')
-            elif "addLiveChatTickerItemAction" in action: #I think this updates the list of recent money-related things to happen in chat recently.
+            elif "removeChatItemAction" in action: # These don't appear in replay files. I assume they're for when mods remove stuff. //confirmed 2025-08-22 20:20
+                print('removeChatItemAction')
+            elif "addLiveChatTickerItemAction" in action: # This is for the "recent things" ticker at the top of the chat. It's a bit redundant because anything that causes a ticker update will also cause an addChatItem.
                 print("TICKER UPDATE WEEWOO WEEWOO WEEWOO")
+            elif "replaceChatItemAction" in action: # 2025-08-22 oh, so that's what it's for
+                print("REPLACEACTION "+action["replaceChatItemAction"]["targetItemId"])
+
+            #I know there will be other types of action and other types of renderer in the addChatItemAction. I've never captured a poll, for instance. That's why I have the else and the except below. They catch the unknowns.
+
             else:
                 print("UNKNOWN ACTION DETECTED! DUMPING ACTION!")
                 print("#######################################")
@@ -121,36 +140,8 @@ while True:
                 exit();
         except Exception as e:
             print("BIG IF FAILED! BAILING!\n")
-           # print(json.dumps(action)[0:100])
             print("\n")
             print(json.dumps(action))
             print("#########################")
             print(traceback.format_exc())
             exit()
-
-        continue # continue statement after if block means the rest of this won't execute'
-        
-        
-        try:
-            if action["addBannerToLiveChatCommand"]:
-                print("BANNER DETECTED! SKIPPING!")
-                continue
-        except KeyError:
-            try:
-                author_name=action["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["authorName"]["simpleText"]
-                print(author_name+": "+actiontomessage(action))
-            except Exception as e:
-                print('{"removeChatItemAction": {"targetItemId": "SomeItemID"}}')
-        
-        #try:
-        #    #if action["addBannerToLiveChatCommand"]:
-        #  #      print("BANNER DETECTED! SKIPPING!")
-        #  #      continue
-        #    author_name=action["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["authorName"]["simpleText"]
-       #    print(author_name)
-        #except Exception as e:
-       #     print("FOR FAILED! NOT BAILING!\n")
-       #     print(json.dumps(action)[0:100])
-        #    print("\n")
-            #print(json.dumps(action))
-        #    exit()
